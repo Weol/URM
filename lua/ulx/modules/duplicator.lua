e8d81f92ac860978ef63a7d2a3fc6b6f615910b2
Erik Rahka <erikrahka@gmail.com>
2016-08-12 03:25:10 +0200

    Adv. Duplicator now respects URM limits

--- lua/ulx/modules/duplicator.lua
+++ lua/ulx/modules/duplicator.lua
@@ -2,7 +2,10 @@
 TIIP = TIIP or {}
 TIIP.URM = TIIP.URM or {}
 
-timer.Simple(5, function() include("ulx/modules/duplicator.lua") end)
+if not TIIP.URM.IsDuplitorLoaded then
+	timer.Simple(5, function() include("ulx/modules/duplicator.lua") end)
+	TIIP.URM.IsDuplitorLoaded = true
+end
 
 local weapons = {
 	"weapon_357",
@@ -56,4 +59,38 @@ end
 
 if AdvDupe2 then
 	hook.Add( "PlayerSpawnEntity", "TIIPURMPlayerSpawnEntity", TIIP.CheckPlayerSpawn, -1 )
+
+	hook.Add( "AdvDupe_FinishPasting", "TIIPURMFinishedPasting", function(data) 
+		local ply = data[1].Player
+		
+		
+		for _,entity in pairs(data[1].CreatedEntities) do 
+		
+			local str = entity:GetClass()
+			model = false
+			
+			if (string.lower(entity:GetClass()) == "prop_physics" or string.lower(entity:GetClass()) == "prop_ragdoll") then
+				str = entity:GetModel()
+				model = true
+			elseif (string.lower(entity:GetClass()) == "prop_effect") then  
+				str = entity:GetTable()[7]
+			elseif (entity:GetTable().VehicleName) then  
+				str = entity:GetTable().VehicleName
+			end
+			
+			if !ply:CheckLimit(str,true) then
+				entity:Remove()
+			elseif not model then	
+				local haslimit, limit = TIIP.URM.HasURMLimit(ply,str)
+				if haslimit then
+					if (type(limit) == "string") then
+						ply:AddCount(limit,entity)
+					end
+				
+					ply:AddCount(haslimit,entity)
+				end
+			end
+			
+		end
+	end, -1 )
 end
